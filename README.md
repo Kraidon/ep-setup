289feab25f6a31724c00ab0057fe340da1a4e44a
Kraidon Epwell <midok.cha1@gmail.com>
2026-09-09 07:57:14 +0300

    - Add pre-install (check directories and config-file, skan link-addresses) - Add installation pkgs (install vim mc btop, used built-in package manager, and install fish and zellij from open internet) - Fix README.md

--- README.md
+++ README.md
@@ -2,29 +2,27 @@
 Script for install packages and plugins on Linux.
 
 ## Platforms
-Distribs:
-- Arch-linux
-- Ubuntu
-Architecture:
-- x86_64
-- aarch64
+| Architecture | ArchLinux | Ubuntu | Debian | Manjaro |
+| :----------- | :-------: | :----: | :----: | :-----: |
+| x86_64       | +         | +      | ?      | ?       |
+| aarch64      | +         | +      | +      | ?       |
 
 ## Packages and Plugins
-1. Packages:
-    1. [vim](https://www.vim.org)
-    2. [mc](https://midnight-commander.org)
-    3. [btop](https://github.com/aristocratos/btop)
-    4. [fish](https://fishshell.com)
-    5. [zellij](https://zellij.dev)
-2. Plug-managers:
-    1. [vim-plug](https://github.com/junegunn/vim-plug)
-    2. [oh-my-fish](https://github.com/oh-my-fish/oh-my-fish)
-3. Vim-plugins:
-    1. [vim-fugitive](https://github.com/tpope/vim-fugitive)
-    2. [vim-surround](https://github.com/tpope/vim-surround)
-    3. [vim-airline](https://github.com/vim-airline/vim-airline)
-    4. [vim-airline-themes](https://github.com/vim-airline/vim-airline-themes)
-    5. [vim-gitgutter](https://github.com/airblade/vim-gitgutter)
+- Packages:
+    - [vim](https://www.vim.org) - text redactor.
+    - [mc](https://midnight-commander.org) - file-manager.
+    - [btop](https://github.com/aristocratos/btop) - system-analyzer.
+    - [fish](https://fishshell.com) (4.9.2) - shell environment.
+    - [zellij](https://zellij.dev) (v0.45.1) - window manager.
+- Plug-managers:
+    - [vim-plug](https://github.com/junegunn/vim-plug) (0.14.0) - plugin manager for vim.
+    - [oh-my-fish](https://github.com/oh-my-fish/oh-my-fish) (v8) - plugin manager for fish.
+- Vim-plugins:
+    - [vim-fugitive](https://github.com/tpope/vim-fugitive)
+    - [vim-surround](https://github.com/tpope/vim-surround)
+    - [vim-airline](https://github.com/vim-airline/vim-airline)
+    - [vim-airline-themes](https://github.com/vim-airline/vim-airline-themes)
+    - [vim-gitgutter](https://github.com/airblade/vim-gitgutter)
 
 ## Installation
 ```sh
--- install.sh
+++ install.sh
@@ -3,8 +3,23 @@
 set -e
 
 SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
+CONFIG_FILE="$SCRIPT_PATH/config.env"
+RESOURCE="$SCRIPT_PATH/.resource"
+RES_archs="$RESOURCE/archives"
+RES_bins="$RESOURCE/bin"
+RES_dirs="$RESOURCE/dirs"
+RES_logs="$RESOURCE/logs"
 
-source "$SCRIPT_PATH/config.env"
+LOG_FILE="$SCRIPT_PATH/log.txt"
+
+if [ -f $LOG_FILE ]; then 
+	rm $LOG_FILE
+fi
+
+touch $LOG_FILE
+
+source /etc/os-release
+source "$CONFIG_FILE"
 
 # Color variables
 Cr_D="\e[0;31m"
@@ -26,32 +41,166 @@ Cw_B="\e[1;37m"
 Cres="\e[0m"
 
 # Log functions
-LOG-task() {
-	TIME="$(date '+%H:%M:%S %D')"
-	
-	echo -e "${Cw_D}[${Cw_B}TASK${Cw_D} ${TIME}] ${Cres}$*"
-}
-
 LOG-comp() {
 	TIME="$(date '+%H:%M:%S %D')"
 	
-	echo -e "${Cg_D}[${Cg_B}COMPLETE${Cg_D} ${TIME}] ${Cres}$*"
+	echo -e "${Cg_D}[${Cg_B}COMPLETE${Cg_D} ${TIME}] ${Cres}$*" | tee -a >(sed -E 's/\x1B\[[0-9;]*[a-zA-Z]//g' >> ${LOG_FILE})
+}
+
+LOG-cmd() {
+	LOG-info "Running: $*"
+	"$@" || LOG-err "Command failed: $*"
 }
 
 LOG-info() {
 	TIME="$(date '+%H:%M:%S %D')"
 	
-	echo -e "${Cw_D}[${Cw_B}INFO${Cw_D} ${TIME}] ${Cres}$*"
+	echo -e "${Cw_D}[${Cw_B}INFO${Cw_D} ${TIME}] ${Cres}$*" | tee -a >(sed -E 's/\x1B\[[0-9;]*[a-zA-Z]//g' >> ${LOG_FILE})
 }
 
 LOG-warn() {
 	TIME="$(date '+%H:%M:%S %D')"
 	
-	echo -e "${Cy_D}[${Cy_B}WARNING${Cy_D} ${TIME}] ${Cres}$*"
+	echo -e "${Cy_D}[${Cy_B}WARNING${Cy_D} ${TIME}] ${Cres}$*" | tee -a >(sed -E 's/\x1B\[[0-9;]*[a-zA-Z]//g' >> ${LOG_FILE})
 }
 
 LOG-err() {
 	TIME="$(date '+%H:%M:%S %D')"
 	
-	echo -e "${Cr_D}[${Cr_B}ERROR${Cr_D} ${TIME}] ${Cres}$*"
+	echo -e "${Cr_D}[${Cr_B}ERROR${Cr_D} ${TIME}] ${Cres}$*" | tee -a >(sed -E 's/\x1B\[[0-9;]*[a-zA-Z]//g' >> ${LOG_FILE})
+	exit 1
 }
+
+pre-install() {	
+	LOG-info "Pre-install"
+	
+	LOG-info "Check config-file..."
+	if [ -f "$CONFIG_FILE" ]; then
+		LOG-comp "File ${CONFIG_FILE} found..."
+	else
+		LOG-err "File ${CONFIG_FILE} not found!!!"
+	fi
+	
+	LOG-info "Please enter password..."
+	sudo -v || LOG-err "Incorrect password!"
+
+	LOG-info "Check link-addresses..."
+#	check-link "$ZJ_URL"
+#	check-link "$FISH_URL"
+#	check-link "$VP_URL"
+#	check-link "$OMF_URL"
+
+	LOG-info "Check directories..."
+	check-dir "$RESOURCE"
+	check-dir "$RES_archs"
+	check-dir "$RES_bins"
+	check-dir "$RES_dirs"
+	check-dir "$RES_logs"
+}
+
+check-dir() {
+	if [ ! -d $1 ]; then
+		LOG-cmd mkdir $1
+	else
+		LOG-info "Directory $1 already created."
+	fi
+}
+
+check-link() {
+	local URL="$1"
+	local CONDITION=$(curl -sLfI --connect-timeout 2 --retry 3 "$URL" > /dev/null)
+
+	if $CONDITION; then
+		LOG-comp "Address $1 worked"
+	else
+		LOG-warn "Address $1 not worked!"
+	fi
+}
+
+install-pkgs() {
+	LOG-info "Installing apps on ${ID}..."
+	case $ID in
+		"arch" )
+			LOG-cmd sudo pacman -Sy > /dev/null
+			LOG-cmd sudo pacman -S "$APPS"
+			;;
+		"ubuntu" | "debian" )
+			LOG-cmd sudo apt-get update
+			LOG-cmd sudo apt-get install -y "${APPS[@]}"
+			;;
+		* )
+			LOG-err "Unknown System."
+			exit 1
+			;;
+	esac
+
+	LOG-info "Installing fish..."
+	local FISH_TAR="${RES_archs}/fish-${FISH_VER}.tar.xz"
+	local FISH_BIN="${RES_bins}/fish"
+	
+	if [ ! -f ${FISH_TAR} ]; then
+		LOG-cmd wget "$FISH_URL" -O "$FISH_TAR"
+	fi
+	if [ ! -f "$FISH_BIN" ]; then
+		LOG-cmd tar -xvf "$FISH_TAR" -C "$RES_bins"
+	fi
+	LOG-cmd chmod u+x "$FISH_BIN"
+	# sudo mv "$FISH_BIN" /usr/local/dir
+
+	LOG-info "installing zellij..."
+	local ZJ_TAR="${RES_archs}/zellij-${ZJ_VER}.tar.gz"
+	local ZJ_BIN="${RES_bins}/zellij"
+	
+	if [ ! -f ${ZJ_TAR} ]; then
+		LOG-cmd wget "$ZJ_URL" -O "$ZJ_TAR"
+	fi
+	if [ ! -f "$ZJ_BIN" ]; then
+		LOG-cmd tar -xvf "$ZJ_TAR" -C "$RES_bins"
+	fi
+	LOG-cmd chmod u+x "$ZJ_BIN"
+	# sudo mv "$ZJ_BIN" /usr/local/dir
+}
+
+install-omf() {
+	LOG-info "Installing Oh-My-Fish."
+	
+	local OMF_TAR="${RES_archs}/omf-${OMF_VER}.tar.gz"
+	local OMF_DIR="${RES_dirs}/oh-my-fish-8"
+
+	if [ ! -f ${OMF_TAR} ]; then
+		LOG-cmd wget "$OMF_URL" -O "$OMF_TAR"
+	fi
+	if [ ! -d "$OMF_DIR" ]; then
+		LOG-cmd tar -xvf "$OMF_TAR" -C "$RES_dirs"
+	fi
+}
+
+install-vp() {
+	LOG-info "Installing Vim-Plug."
+	
+	local VP_TAR="${RES_archs}/vp-${VP_VER}.tar.gz"
+	local VP_DIR="${RES_dirs}/vim-plug-0.14.0"
+
+	if [ ! -f ${VP_TAR} ]; then 
+		LOG-cmd wget "$VP_URL" -O "$VP_TAR"
+	fi
+	if [ ! -d "$VP_DIR" ]; then
+		LOG-cmd tar -xvf "$VP_TAR" -C "$RES_dirs"
+	fi
+}
+
+post-install() {
+	LOG-info "Post-install"
+	
+	LOG-info "Remove directory $RESOURCES"
+	if [ -d "$RESOURCES" ]; then
+		rm -rf "$RESOURCES"
+	else
+		LOG-info "Directory $RESOURCES no found!"
+	fi
+}
+
+pre-install
+install-pkgs
+install-omf
+install-vp
